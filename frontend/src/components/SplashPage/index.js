dfa76fc72efc73826380cb6afef2b4f0f90868e7
james <monaco.james18@gmail.com>
2021-03-03 15:57:27 -0600

    added splash + some styling

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -3,7 +3,12 @@ import "./splash.css"
 
 function SplashPage () {
     return (
-        <h1>hello</h1>
+        <>
+            <div className="splash__div_container">
+                <h1>Welcome to pet-reads!</h1>
+                <h1>Find the best books about how to take care of your pet! Click the sign up button to create an account or click home to see the our books!</h1>
+            </div>
+        </>
     )
 }
 
