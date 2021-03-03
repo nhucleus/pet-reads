4a6f5931395957176837a0897c20d6eb2c334815
james <monaco.james18@gmail.com>
2021-03-03 15:16:16 -0600

    react boiler plate added and added route to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+import SplashPage from "./components/SplashPage";
 
 function App() {
   const dispatch = useDispatch();
@@ -23,6 +24,9 @@ function App() {
           </Route>
           <Route path="/signup">
             <SignupFormPage />
+          </Route>
+          <Route path="/" exact>
+            <SplashPage />
           </Route>
         </Switch>
       )}
---
+++ frontend/src/components/SplashPage/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./splash.css"
+
+function SplashPage () {
+    return (
+        <h1>hello</h1>
+    )
+}
+
+export default SplashPage;
