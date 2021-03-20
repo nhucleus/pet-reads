6adde0c278102cde36a7e5e1dcec33dae8ac1faf
Nhu-y Phan <nhucleus@gmail.com>
2021-03-20 18:12:25 -0400

    footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -23,17 +23,9 @@ const Footer = () => {
           <div className="modal-title">
             Contributors
           </div>
-          <div className="modal-left">
-            <div className="modal-photos-container">
-            <a className="modal-links" href="https://nhucleus.io/" target="_blank"><img draggable="false" className="modal-photos" src="https://avatars.githubusercontent.com/u/70211658?s=460&v=4" /></a>
-            <a className="modal-links" href="https://monaco021.github.io/" target="_blank"><img draggable="false" className="modal-photos" src="https://avatars.githubusercontent.com/u/70599412?s=460&u=9d760acc7ca3e2a0c16b52cbdc6a46fc4e560a43&v=4" /></a>
-            </div>
-          </div>
-          <div className="modal-right">
-          <div className="modal-links-container">
-            <a className="modal-links" href="https://nhucleus.io/" target="_blank">Nhu-y Phan</a>
-            <a className="modal-links" href="https://monaco021.github.io/" target="_blank">James Monaco</a>
-            </div>
+          <div className="modal-info">
+            <a className="modal-links" href="https://nhucleus.io/" target="_blank"><img draggable="false" className="modal-photos" src="https://avatars.githubusercontent.com/u/70211658?s=460&v=4" /><span>Nhu-y Phan</span></a>
+            <a className="modal-links" href="https://monaco021.github.io/" target="_blank"><img draggable="false" className="modal-photos" src="https://avatars.githubusercontent.com/u/70599412?s=460&u=9d760acc7ca3e2a0c16b52cbdc6a46fc4e560a43&v=4" /><span>James Monaco</span></a>
           </div>
         </div>
       </Modal>
@@ -42,17 +34,9 @@ const Footer = () => {
           <div className="modal-title">
             LinkedIn
           </div>
-          <div className="modal-left">
-          <div className="modal-photos-container">
-            <a className="modal-links" href="https://www.linkedin.com/in/nhu-yphan/" target="_blank"><img draggable="false" className="modal-photos" src="https://avatars.githubusercontent.com/u/70211658?s=460&v=4" /></a>
-            <a className="modal-links" href="https://www.linkedin.com/in/james-monaco-770127175/" target="_blank"><img draggable="false" className="modal-photos" src="https://avatars.githubusercontent.com/u/70599412?s=460&u=9d760acc7ca3e2a0c16b52cbdc6a46fc4e560a43&v=4" /></a>
-            </div>
-          </div>
-          <div className="modal-right">
-            <div className="modal-links-container">
-              <a className="modal-links" href="https://www.linkedin.com/in/nhu-yphan/" target="_blank">Nhu-y Phan</a>
-              <a className="modal-links" href="https://www.linkedin.com/in/james-monaco-770127175/" target="_blank">James Monaco</a>
-            </div>
+          <div className="modal-info">
+            <a className="modal-links" href="https://www.linkedin.com/in/nhu-yphan/" target="_blank"><img draggable="false" className="modal-photos" src="https://avatars.githubusercontent.com/u/70211658?s=460&v=4" /><span>Nhu-y Phan</span></a>
+            <a className="modal-links" href="https://www.linkedin.com/in/james-monaco-770127175/" target="_blank"><img draggable="false" className="modal-photos" src="https://avatars.githubusercontent.com/u/70599412?s=460&u=9d760acc7ca3e2a0c16b52cbdc6a46fc4e560a43&v=4" /><span>James Monaco</span></a>
           </div>
         </div>
       </Modal>
