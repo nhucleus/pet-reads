9407557d7a01b373a10a1fde62deca795e0ce5f0
Nhu-y Phan <nhucleus@gmail.com>
2021-06-15 13:16:11 -0700

    div

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -4,13 +4,13 @@ import "./Modal.css";
 const Modal = ({ open, children, onClose }) => {
   if (!open) return null;
   return ReactDOM.createPortal(
-    <>
+    <div>
       <div className="modal-overlay" onClick={ onClose }></div>
       <div className="modal">
         {children}
       </div>
       
-    </>,
+    </div>,
     document.getElementById("modal-portal")
   );
 };
