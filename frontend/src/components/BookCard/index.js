e73dbf0ee045b6320854af13dbd3d22c53fe4c57
Nhu-y Phan <nhucleus@gmail.com>
2021-06-15 13:20:10 -0700

    alt

--- frontend/src/components/BookCard/index.js
+++ frontend/src/components/BookCard/index.js
@@ -7,21 +7,24 @@ const BookCard = ({ book }) => {
     
     
     return (
-        <div className="book-card-container" onClick={() => {
-            history.push(`/book/${book.id}`);
-        }}>
-            <img draggable={false} className="book-card-cover" src={book.bookCover} />
-            <div className="book-card-title">
-                {book.title}
-            </div>
-            <div className="book-card-author">
-                {book.Author.name}
-            </div>
-            <div className="book-card-rating">
-                <RatingSystem rating={book.avgRating}/>
-            </div>
+      <div
+        className="book-card-container"
+        onClick={() => {
+          history.push(`/book/${book.id}`);
+        }}
+      >
+        <img
+          draggable={false}
+          className="book-card-cover"
+          src={book.bookCover}
+          alt="Book cover"
+        />
+        <div className="book-card-title">{book.title}</div>
+        <div className="book-card-author">{book.Author.name}</div>
+        <div className="book-card-rating">
+          <RatingSystem rating={book.avgRating} />
         </div>
-
+      </div>
     );
 };
 
